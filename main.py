c2c9524757084455f6667d41935621886483dede
codexdebayan <cdebayan24@gmail.com>
2024-08-30 12:50:19 +0530

    feat : Added tests

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello world")
+class Hello:
+    @staticmethod
+    def hello_world():
+        return "Hello world"
+
+if __name__ == '__main__':
+    greeting = Hello.hello_world()
+    print(greeting)
